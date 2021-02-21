114312c38f0ca3d40a88abe3d6290734511f13bf
Cocano <Cocano@COCANO_PC>
2021-02-21 13:50:22 -0600

    listas

---
+++ ProyeProgra6/Controllers/ServicioOProductoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyeProgra6.Models;
+
+namespace ProyeProgra6.Controllers
+{
+    public class ServicioOProductoController : Controller
+    {
+        proyectoprogra6Entities modeloBD = new proyectoprogra6Entities();
+
+        public ActionResult ServicionoProductoLista()
+        {
+            //creamos la variable que contiene los registros obtenidos
+            ///al invocar los procesos almacenados
+            List<sp_RetornaServicioOProducto_Result> modeloVista =
+                new List<sp_RetornaServicioOProducto_Result>();
+
+            ///asignar a la variable el resultado de 'llamas el procedimiento almacenado
+
+            modeloVista = this.modeloBD.sp_RetornaServicioOProducto("", "").ToList();
+            //enviar a la vista el modelo
+            return View(modeloVista);
+
+        }
+    }
+}
---
+++ ProyeProgra6/Controllers/TiposVehiculosController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyeProgra6.Models;
+
+namespace ProyeProgra6.Controllers
+{
+    public class TiposVehiculosController : Controller
+    {
+        
+
+        proyectoprogra6Entities modeloBD = new proyectoprogra6Entities();
+
+        public ActionResult TiposVehiculosLista()
+        {
+            //creamos la variable que contiene los registros obtenidos
+            ///al invocar los procesos almacenados
+            List<sp_RetornaTiposVehículos_Result> modeloVista =
+                new List<sp_RetornaTiposVehículos_Result>();
+
+            ///asignar a la variable el resultado de 'llamas el procedimiento almacenado
+
+            modeloVista = this.modeloBD.sp_RetornaTiposVehículos("", "").ToList();
+            //enviar a la vista el modelo
+            return View(modeloVista);
+
+        }
+    }
+}
