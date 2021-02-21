bc9e7004d1b77a3ce1fed682a701b63a93d462e7
Cocano <Cocano@COCANO_PC>
2021-02-21 16:22:44 -0600

    agregar controller

--- ProyeProgra6/Controllers/UsuariosController.cs
+++ ProyeProgra6/Controllers/UsuariosController.cs
@@ -27,6 +27,92 @@ namespace ProyeProgra6.Controllers
 
         }
 
+        ////--------------------------------------------------------------------------------------------
+        ///
+        /// 
+
+        public ActionResult UsuarioNuevo()
+        {
+            AgregaProvinciasViewBag();
+            AgregaCantonesViewBag();
+            AgregaDistritosViewgBag();
+            return View();
+        }
+
+        /// //agrega provincias al viewbag
+        /// <summary>
+        /// para que sean accedidas desde la vista
+        /// es case sensitive
+        /// </summary>
+        void AgregaProvinciasViewBag()
+        {
+            this.ViewBag.UsuariosLista =
+                    this.modeloBD.sp_RetornaProvincias("").ToList();
+        }
+
+        void AgregaCantonesViewBag()
+        {
+            this.ViewBag.UsuariosLista =
+                    this.modeloBD.sp_RetornaCantones("",null).ToList();
+        
+        }
+
+        void AgregaDistritosViewgBag()
+        {
+            this.ViewBag.UsuariosLista =
+                this.modeloBD.sp_RetornaDistritos("", null);
+        }
+        /// <summary>
+        /// este envia la persona nueva
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult UsuarioNuevo(sp_RetornaUsuarios_Result modeloVista)
+        {
+            ///registra cantidad de registros afectados.
+            ///
+            int cantRegistrosafectados = 0;
+            string resultado = "";
+
+            try
+            {
+                cantRegistrosafectados =
+                    this.modeloBD.sp_InsertaPersona(
+                        modeloVista.primerApellido,
+                        modeloVista.segundoApellido,
+                        modeloVista.nombre,
+                        modeloVista.telefono,
+                        modeloVista.correo,
+                        modeloVista.id_Provincia,
+                        modeloVista.sexo,
+                        modeloVista.poseeCarro,
+                        modeloVista.poseeBici,
+                        modeloVista.poseeMoto
+                        );
+            }
+            catch (Exception error)
+            {
+                resultado = "Ocurrió un error:" + error.Message;
+
+            }
+            finally
+            {
+                if (cantRegistrosafectados > 0)
+                {
+                    resultado = "Registro insertado";
+
+                }
+                else
+                {
+                    resultado += ".No se Pudo insertas";
+                }
+            }
+
+            Response.Write("<script language=javascript>alert('" + resultado + "');</script>");
+            this.AgregaProvinciasViewBag();
+            return View();
+        }
+
 
     }
 }
