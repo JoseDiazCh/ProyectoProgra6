1a99713beae22ccd6e9cfd753338247a87f69178
Cocano <Cocano@COCANO_PC>
2021-02-21 15:14:56 -0600

    listas completas

---
+++ ProyeProgra6/Controllers/CantonesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyeProgra6.Models;
+
+namespace ProyeProgra6.Controllers
+{
+    public class CantonesController : Controller
+    {
+        proyectoprogra6Entities modeloBD = new proyectoprogra6Entities();
+
+        public ActionResult CantonesLista()
+        {
+            //creamos la variable que contiene los registros obtenidos
+            ///al invocar los procesos almacenados
+            List<sp_RetornaCantones_Result> modeloVista =
+                new List<sp_RetornaCantones_Result>();
+
+            ///asignar a la variable el resultado de 'llamas el procedimiento almacenado
+
+            modeloVista = this.modeloBD.sp_RetornaCantones("", null).ToList();
+            //enviar a la vista el modelo
+            return View(modeloVista);
+        }
+    }
+}
---
+++ ProyeProgra6/Controllers/DistritosController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyeProgra6.Models;
+
+namespace ProyeProgra6.Controllers
+{
+    public class DistritosController : Controller
+    {
+        proyectoprogra6Entities modeloBD = new proyectoprogra6Entities();
+
+        public ActionResult DistritosLista()
+        {
+            //creamos la variable que contiene los registros obtenidos
+            ///al invocar los procesos almacenados
+            List<sp_RetornaDistritos_Result> modeloVista =
+                new List<sp_RetornaDistritos_Result>();
+
+            ///asignar a la variable el resultado de 'llamas el procedimiento almacenado
+
+            modeloVista = this.modeloBD.sp_RetornaDistritos("", null).ToList();
+            //enviar a la vista el modelo
+            return View(modeloVista);
+        }
+    }
+}
