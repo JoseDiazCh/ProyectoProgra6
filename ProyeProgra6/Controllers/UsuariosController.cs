32b09926e7fa4f209003d1bee53be26ad69ecfa3
Cocano <Cocano@COCANO_PC>
2021-02-21 12:49:35 -0600

    lista usuarios

--- ProyeProgra6/Controllers/UsuariosController.cs
+++ ProyeProgra6/Controllers/UsuariosController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ProyeProgra6.Models;
 
 namespace ProyeProgra6.Controllers
 {
@@ -13,5 +14,24 @@ namespace ProyeProgra6.Controllers
         {
             return View();
         }
+
+        proyectoprogra6Entities modeloBD = new proyectoprogra6Entities();
+
+        public ActionResult UsuariosLista()
+        {
+            //creamos la variable que contiene los registros obtenidos
+            ///al invocar los procesos almacenados
+            List<sp_RetornaUsuarios_Result> modeloVista =
+                new List<sp_RetornaUsuarios_Result>();
+
+            ///asignar a la variable el resultado de 'llamas el procedimiento almacenado
+
+            modeloVista = this.modeloBD.sp_RetornaUsuarios("", "").ToList();
+            //enviar a la vista el modelo
+            return View(modeloVista);
+
+        }
+
+
     }
 }
