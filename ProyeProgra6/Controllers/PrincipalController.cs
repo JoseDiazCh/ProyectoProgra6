dc887aa28259584213c2ba8b28e34e821f466d85
Cocano <Cocano@COCANO_PC>
2021-04-05 21:44:21 -0600

    Pagina Principal

---
+++ ProyeProgra6/Controllers/PrincipalController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyeProgra6.Models;
+
+namespace ProyeProgra6.Controllers
+{
+    public class PrincipalController : Controller
+    {
+
+        proyectoprogra6Entities modeloBD = new proyectoprogra6Entities();
+
+        // GET: Principal
+        public ActionResult PaginaPrincipal()
+        {
+            return View();
+        }
+    }
+}
