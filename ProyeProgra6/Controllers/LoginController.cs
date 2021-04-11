d5fdab4d509ed364d8794f725da153ada55bafce
JOSE DANIEL DIAZ CHACON <117050320@castrocarazo.ac.cr>
2021-04-10 20:57:15 -0600

    login

---
+++ ProyeProgra6/Controllers/LoginController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProyeProgra6.Models;
+
+namespace ProyeProgra6.Controllers
+{
+    public class LoginController : Controller
+    {
+        /// <summary>
+        /// coneccion a la base de datos
+        /// </summary>
+        proyectoprogra6Entities modeloBD = new proyectoprogra6Entities();
+
+        // GET: Login
+        public ActionResult InicioSesion()
+        {
+            return View();
+        }
+
+        public ActionResult Validar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Validar(string correo, string contrasenia)
+        {
+            Session["correoVista"] = correo;
+            string url = "";
+            string mensaje = "";
+            try
+            {
+                sp_RetornaUsuarios_Result datosCliente = new sp_RetornaUsuarios_Result();
+                datosCliente = modeloBD.sp_RetornaUsuarios(correo, contrasenia).FirstOrDefault();
+
+                if (datosCliente == null)
+                {
+                    Response.Write("<script>alert('Datos Invalidos')</script>"); ;
+                    this.Session.Add("Nombre", null);
+                    this.Session.Add("Tipo", null);
+                    this.Session.Add("Fecha", null);
+                    this.Session.Add("UsuarioLogeado", null);
+                }
+                else
+                {
+
+                    Session.Add("Nombre", datosCliente.@Nombre);
+                    Session.Add("Tipo", datosCliente.@TipoUsuario);
+                    Session.Add("Fecha", datosCliente.@FechaNacimiento);
+                    Session.Add("UsuarioLogeado", true);
+
+                    if (Convert.ToInt32(this.Session["Tipo"]) == 1)
+                    {
+                        url = Url.Action("Inicio", "Home");
+                        //Response.Redirect("~/Factura/ListaPreFactura");
+                    }
+                    else if (Convert.ToInt32(this.Session["Tipo"]) == 2)
+                    {
+                        url = Url.Action("ClientesVistaLista", "ClienteVista");
+                        //this.Response.Redirect("~/Servicios/ServicioLista");
+                    }
+                    mensaje = "Usted ha ingresado: " + Session["Nombre"];
+                }
+            }
+            catch (Exception excepcionCapturada)
+            {
+                mensaje += $"Ocurrio un error:{excepcionCapturada.Message}";
+            }
+
+            return Json(new { resultado = mensaje, validado = Session["UsuarioLogeado"], url = url });
+
+
+        }
+    }
+}
---
+++ ProyeProgra6/Scripts/Login/Login.js
@@ -0,0 +1,64 @@
+﻿$(function () {
+    creaValidaciones();
+    creaEventos();
+});
+
+///crea las validaciones para el formulario
+function creaValidaciones() {
+    $("#frmEjemploPost").validate({
+        ///objeto que contiene "las condiciones" que el formulario
+        ///debe cumplir para ser considerado válido
+        rules: {
+            correo: {
+                required: true
+            },
+            contrasenia: {
+                required: true
+            },
+        }
+    });
+}
+
+function creaEventos() {
+    $("#btnAceptar").on("click", function () {
+        var formulario = $('#frmEjemploPost');
+        formulario.validate();
+
+        if (formulario.valid()) {
+            invocarMetodoPost();
+        }
+    });
+}
+///se encarga de llamar al método del controlador y procesar el resultado
+function invocarMetodoPost() {
+    var url = '/Login/Validar';
+
+    var parametros = {
+        correo: $("#correo").val(),
+        contra: $("#contrasenia").val(),
+    };
+    Ajax(url, parametros);
+}
+
+function Ajax(url, parametros) {
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        type: 'post',
+        contentType: 'application/json',
+        data: JSON.stringify(parametros),
+        success: function (data, textStatus, jQxhr) {
+            procesarResultadoMetodo(data);
+        },
+        error: function (jQxhr, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+}
+
+function procesarResultadoMetodo(data) {
+    var resultadoFuncion = data;
+    if (resultadoFuncion.validado == true) {
+        window.location.href = resultadoFuncion.url;
+    }
+}
