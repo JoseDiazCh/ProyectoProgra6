07cd7712cf65a98d9609814d7d91e8be53ba1d08
Cocano <Cocano@COCANO_PC>
2021-04-11 19:18:58 -0600

    js Login

---
+++ ProyeProgra6/Scripts/Login/LoginPaginaPrincipal.js
@@ -0,0 +1,85 @@
+﻿$(function () {
+    creaValidaciones();
+    creaEventos();
+});
+
+///crea las validaciones para el formulario
+function creaValidaciones() {
+    $("#frmIniciodeSesion").validate({
+        ///objeto que contiene "las condiciones" que el formulario
+        ///debe cumplir para ser considerado válido
+        rules: {
+            correo: {
+                required: true
+            },
+            contrasenia: {
+                required: true
+            },
+        }
+    });
+}
+///llamar eventos al iniciar secion
+function creaEventos() {
+    $("#btnAceptar").on("click", function () {
+        var formulario = $('#frmIniciodeSesion');
+        formulario.validate();
+
+        if (formulario.valid()) {
+            invocarMetodoPost();
+        }
+    });
+}
+///se encarga de llamar al metodo del controlador y procesar el resultado
+///pasamos los parametros ingresados por el usuario
+function invocarMetodoPost() {
+    var url = '/Home/ValidarUsuario';
+
+    var parametros = {
+        pcorreo: $("#correo").val(),
+        pcontrasenia: $("#contrasenia").val(),
+    };
+
+    ///invocar al metodo
+    $.ajax({
+        url: url,
+        dataType: 'json',
+        type: 'post',
+        contentType: 'application/json',
+        data: JSON.stringify(parametros),
+        success: function (data, textStatus, jQxhr) {
+            procesarResultadoMetodo(data);
+        },
+        error: function (jQxhr, textStatus, errorThrown) {
+            alert(errorThrown);
+        }
+    });
+}
+
+
+///apartir de la validacion de usuario desde el server
+///Iniciar secion o mandar msj de error
+function procesarResultadoMetodo(data) {
+
+    var resultadoFuncion = data.resultado;
+
+    if (resultadoFuncion.validado == true) {
+
+        cargarDatosUsuario();
+        $("#myModal").modal();
+
+        setTimeout(function () {
+            window.location.href = '/Home/PaginaPrincipal';
+        }, 5000);
+    }
+    else {
+        $("#myModal1").modal;
+    }
+}
+///metodo que modifica la etiqueta por id bienvenida
+///muestra el nombre del usuario que inicion secion
+function procesarResultadoUsuario(data) {
+    var resultadoFuntion = data.resultado;
+    var ddlTesto = $("#Bienvenida");
+    ddlTesto.text("Bienvenido al sistema: " + resultadoFuntion);
+
+}
